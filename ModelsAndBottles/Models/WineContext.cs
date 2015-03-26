e6b3951402b49ca14eb18db87e94b7c6024e645c
gwake1 <gwake1@gmail.com>
2015-03-26 09:44:09 -0500

    Wine Context

---
+++ ModelsAndBottles/Models/WineContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ModelsAndBottles.Models
+{
+    public class WineContext: DbContext
+    {
+        public DbSet<WineList> WineLists { get; set; }
+        public DbSet<Terroir> Terroirs { get; set; }
+        public DbSet<WineVintage> WineVintages { get; set; }
+        public DbSet<Wine> Wines { get; set; } 
+    }
+}
---
+++ ModelsAndBottles/Models/WineVintage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace ModelsAndBottles.Models
+{
+    public class WineVintage
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public int WineListId { get; set; }
+        [Required]
+        public string Maturity { get; set; }
+        [Required]
+        public int Rating { get; set; }
+
+        // Wine Foreign Key
+        public int WineId { get; set; }
+        // Wine Navigation Property
+        public Wine Wine { get; set; }
+    }
+}
